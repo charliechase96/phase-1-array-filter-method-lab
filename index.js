5688feab1e082432049aaea9c2eafde477db04b4
Charlie Gray <charliechasegray@gmail.com>
2023-02-07 17:26:35 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solution here
+function findMatching(drivers, name) {
+    return drivers.filter(function (driver) {
+        return driver.toUpperCase() === name.toUpperCase();
+    })
+}
+
+function fuzzyMatch(drivers, name) {
+    return drivers.filter(function (driver) {
+        return driver.toUpperCase().substring(0, name.length) === name.toUpperCase();
+    })
+}
+
+function matchName(drivers, input) {
+    return drivers.filter(function (driver) {
+        return driver.name === input;
+    });
+}
